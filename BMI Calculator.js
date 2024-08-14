338b58cc8ae570306e2beb2a0b84ed717b38fd1d
unknown <enkhmunkh@att.net>
2024-08-14 09:17:21 -0500

    Created isLeap year function

--- BMI Calculator.js
+++ BMI Calculator.js
@@ -1,8 +1,17 @@
 //BMI calculator = weight(kg) / height m squared (m2)
 
-function bmiCalc (w, h) {
+function bmiCalc (weight, height) {
+    let bmi = Math.round(weight/(Math.pow(height, 2)));
 
-    return(Math.round(w/(Math.pow(h, 2))));
+    if (bmi < 18.5) {
+        return "Your BMI is " + bmi + ", so you are underweight."
+    }
+    else if (bmi > 24.9) {
+        return "Your BMI is " + bmi + ", so you are overweight."
+    }
+    else {
+        return "Your BMI is " + bmi + ", so you have a normal weight."
+    }
 };
 
 
---
+++ isLeap.js
@@ -0,0 +1,15 @@
+function isLeap(year) {
+    if (year % 4 === 0)
+    {
+        if ( year % 100 === 0) {
+            if(year % 400 === 0){
+                return true;
+            }
+            return false;
+        }
+        return  true;
+    }    
+}
+
+
+console.log(isLeap(2100));
